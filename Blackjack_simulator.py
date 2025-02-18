41efdcc3ab94f1eb610e3b9d44b31a5b37700cbd
Sachiel <sach@bu.edu>
2025-02-18 12:37:13 -0500

    Changed reward to actually play the game against a dealer

--- Blackjack_simulator.py
+++ Blackjack_simulator.py
@@ -7,37 +7,78 @@ def draw():
 def deal():
     hand = [draw(), draw()]
     return sum(hand), hand
-def Naive_blackjack():
+def Play_blackjack(player_type = 'Player'): #Lazy implementation, probably could be much more consice but the code is functional
     ##starting hand
-    aces = 0
-    total = random.randint(2,11)
-    if total == 11:
-        aces = 1
-    drawn = draw()
-    if drawn == 11:
-        aces += 1
-    while total < 17:
-        total += drawn
-        if total > 21 and aces > 0:
-            total -= 10
-            aces -= 1
+    if player_type == 'Player':
+        aces = 0
+        total = draw()
+        if total == 11:
+            aces = 1
         drawn = draw()
         if drawn == 11:
             aces += 1
-    if total > 21:
-        return 'Bust'
-    return total 
-
+        while total < 17:
+            total += drawn
+            if total > 21 and aces > 0:
+                total -= 10
+                aces -= 1
+            drawn = draw()
+            if drawn == 11:
+                aces += 1
+        if total > 21:
+            return 0
+        return total 
+    
+    elif player_type == 'Dealer':
+        aces = 0
+        hand = deal()
+        total = hand[0]
+        for item in hand[1]:
+            if item == 11:
+                aces += 1
+        open_card = hand[1][0]
+        while total < 17:
+            total += drawn
+            if total > 21 and aces > 0:
+                total -= 10
+                aces -= 1
+            drawn = draw()
+            if drawn == 11:
+                aces += 1
+        if total > 21:
+            return 0
+        return total, open_card
+    else:
+        return("Invalid player type, should be 'Player' or 'Dealer'!")
+    
+def dealer_from_hand(hand):
+        aces = 0
+        total = hand
+        if hand == 11:
+            aces = 1
+        while total < 17:
+            if total > 21 and aces > 0:
+                total -= 10
+                aces -= 1
+            drawn = draw()
+            if drawn == 11:
+                aces += 1
+            total += drawn
+        if total > 21:
+            return 0
+        return total
 
 def simulate_blackjack(n):
-    results = {}
+    results = {} 
     for i in range(n):
-        play = (Naive_blackjack())
+        play = (Play_blackjack())
         if play in results:
             results[play] += 1
         else:
             results[play] = 1
     return (results)
+
+
 
 simulate_blackjack(100000)
 
